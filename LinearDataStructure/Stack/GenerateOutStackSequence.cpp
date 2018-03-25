f1a357d94019c6aa0554984fb3a2c0286325f850
cjhahaha <cj_lee_canu@hotmail.com>
2018-03-25 16:11:49 +0800

    update format

--- LinearDataStructure/Stack/GenerateOutStackSequence.cpp
+++ LinearDataStructure/Stack/GenerateOutStackSequence.cpp
@@ -11,30 +11,30 @@ char str[MAXL], stack[MAXL];
 int len;
 
 void generate(int str_head, char * stack, int stack_head, string out_seq) {
-	if (out_seq.length() == len) {
-		cout << out_seq << endl;
-		return;
-	}
+    if (out_seq.length() == len) {
+        cout << out_seq << endl;
+        return;
+    }
 
-	if (str_head < len) {                                       // if thers exists element nerver in stack
-		stack[stack_head] = str[str_head];                      // push this element into stack
-		generate(str_head + 1, stack, stack_head + 1, out_seq);
-	}
+    if (str_head < len) {                                       // if thers exists element nerver in stack
+        stack[stack_head] = str[str_head];                      // push this element into stack
+        generate(str_head + 1, stack, stack_head + 1, out_seq);
+    }
 
-	if (stack_head > 0) {                                       // if stack is not empty
-		char ch = stack[stack_head - 1];                        // make backup
+    if (stack_head > 0) {                                       // if stack is not empty
+        char ch = stack[stack_head - 1];                        // make backup
 
-		out_seq.push_back(stack[stack_head - 1]);               // pop head element
-		generate(str_head, stack, stack_head - 1, out_seq);
+        out_seq.push_back(stack[stack_head - 1]);               // pop head element
+        generate(str_head, stack, stack_head - 1, out_seq);
 
-		stack[stack_head - 1] = ch;                             // restore
-	}
+        stack[stack_head - 1] = ch;                             // restore
+    }
 }
 
 
 int main() {
-	strcpy(str, "ABCD");
-	len = strlen(str);
+    strcpy(str, "ABCD");
+    len = strlen(str);
 
-	generate(0, stack, 0, "");
+    generate(0, stack, 0, "");
  }
--- LinearDataStructure/Stack/ReversePolishNotation.cpp
+++ LinearDataStructure/Stack/ReversePolishNotation.cpp
@@ -7,59 +7,59 @@ using namespace std;
 typedef long long ll;
 
 int judge_priority(char ch1, char ch2) {
-	if ((ch1 == '+' || ch1 == '-') && (ch2 == '+' || ch2 == '-')) return 0;
-	if ((ch1 == '+' || ch1 == '-') && (ch2 == '*' || ch2 == '/')) return 1;
-	if ((ch1 == '*' || ch1 == '/') && (ch2 == '*' || ch2 == '/')) return 0;
-	if ((ch1 == '*' || ch1 == '/') && (ch2 == '+' || ch2 == '-')) return 0;
-	if (ch1 == '(') return 1;
-	return 0;
+    if ((ch1 == '+' || ch1 == '-') && (ch2 == '+' || ch2 == '-')) return 0;
+    if ((ch1 == '+' || ch1 == '-') && (ch2 == '*' || ch2 == '/')) return 1;
+    if ((ch1 == '*' || ch1 == '/') && (ch2 == '*' || ch2 == '/')) return 0;
+    if ((ch1 == '*' || ch1 == '/') && (ch2 == '+' || ch2 == '-')) return 0;
+    if (ch1 == '(') return 1;
+    return 0;
 }
 
 
 void covert_to_RPN(string exp) {
-	stack<char> s;                               // stack storing operators
+    stack<char> s;                               // stack storing operators
 
-	for (char ch: exp) {
-		if (is_num(ch) || ch == '.')             // if ch is digit
-			putchar(ch);
-		else if (ch == '(' || s.empty())         // if ch is '(' or the stack is empty
-			s.push(ch);                          // directly push ch into stack
-		else if (ch == ')') {
-			while (not s.empty()) {              // output all operators between the first '('
-				putchar(s.top());
-				s.pop();
+    for (char ch: exp) {
+        if (is_num(ch) || ch == '.')             // if ch is digit
+            putchar(ch);
+        else if (ch == '(' || s.empty())         // if ch is '(' or the stack is empty
+            s.push(ch);                          // directly push ch into stack
+        else if (ch == ')') {
+            while (not s.empty()) {              // output all operators between the first '('
+                putchar(s.top());
+                s.pop();
 
-				if (s.top() == '(') {
-					s.pop();
-					break;
-				}
-			}
-		}
-		else {                                   // if ch is '+-*/'
-			int t = judge_priority(s.top(), ch);
-			if (t == 1)                          // if the top operator in the stack has lower priority than current operator
-				s.push(ch);
-			else {
-				while (not s.empty() && judge_priority(s.top(), ch) == 0) {
-					putchar(s.top());
-					s.pop();
-				}
+                if (s.top() == '(') {
+                    s.pop();
+                    break;
+                }
+            }
+        }
+        else {                                   // if ch is '+-*/'
+            int t = judge_priority(s.top(), ch);
+            if (t == 1)                          // if the top operator in the stack has lower priority than current operator
+                s.push(ch);
+            else {
+                while (not s.empty() && judge_priority(s.top(), ch) == 0) {
+                    putchar(s.top());
+                    s.pop();
+                }
 
-				s.push(ch);
-			}
-		}
-	
-	}
+                s.push(ch);
+            }
+        }
+    
+    }
 
-	while (not s.empty()) {
-		putchar(s.top());
-		s.pop();
-	}
+    while (not s.empty()) {
+        putchar(s.top());
+        s.pop();
+    }
 
-	cout << endl;
+    cout << endl;
 }
 
 
 int main() {
-	covert_to_RPN("(3*5+3-2+32)*6+4/5");
+    covert_to_RPN("(3*5+3-2+32)*6+4/5");
 }
