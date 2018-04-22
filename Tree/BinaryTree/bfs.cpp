4816c8540422669d472e199240801f135dadfa97
cjhahaha <cj_lee_canu@hotmail.com>
2018-04-22 15:33:31 +0800

    add bfs.cpp

---
+++ Tree/BinaryTree/bfs.cpp
@@ -0,0 +1,28 @@
+#include <queue>
+#include <string>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+struct Node {
+    char data;
+    Node * l, * r;
+
+    Node(char _data = ' ') { data = _data; l = r = NULL; }
+};
+
+
+Node * bfs(Node * root, char data) {
+    queue<Node *> q; q.push(root);
+    while (not q.empty()) {
+        Node * cur = q.front(); q.pop();
+		if (cur -> data == data) return cur;
+        if (cur -> l) q.push(cur -> l);
+        if (cur -> r) q.push(cur -> r);
+    }
+	return NULL;
+}
+
+int main() {
+
+}
