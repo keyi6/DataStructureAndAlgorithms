e4ea71f00b818fa727300a01212f2d0d29ea7f6b
cjhahaha <cj_lee_canu@hotmail.com>
2018-04-22 13:51:56 +0800

    add judge_complete_binary_tree.cpp

---
+++ Tree/BinaryTree/judge_complete_binary_tree.cpp
@@ -0,0 +1,51 @@
+#include <queue>
+#include <vector>
+#include <string>
+#include <cstring>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+typedef long long ll;
+
+struct Node {
+    char data;
+    Node * l, * r;
+
+    Node(char _data = ' ') { data = _data; l = r = NULL; }
+};
+
+void build_tree(Node * & root) {
+    char ch;
+    cin >> ch;
+
+    if (ch == '#') return;
+    root = new Node(ch);
+
+    build_tree(root -> l);
+    build_tree(root -> r);
+}
+
+bool is_complete_binary_tree(Node * root) {
+    bool flag = false;
+    queue<Node *> q;
+    q.push(root);
+    while (not q.empty()) {
+        Node * cur = q.front();
+        q.pop();
+
+        if (cur -> r && ! cur -> l) return false;
+        if (! cur -> l && ! cur -> r) flag = true;
+        else if (flag && (cur -> l || cur -> r)) return false;
+
+        if (cur -> l) q.push(cur -> l);
+        if (cur -> r) q.push(cur -> r);
+    }
+
+    return true;
+}
+
+int main() {
+    Node * root = NULL;
+    build_tree(root);
+    cout << (is_complete_binary_tree(root) ? "Yes" : "No") << endl;
+}
