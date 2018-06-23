a0759d40f3f0a72e927f1c37b705ab20072370b8
cjhahaha <cj_lee_canu@hotmail.com>
2018-06-23 18:47:09 +0800

    fix few bugs

--- homework/3/3.cpp
+++ homework/3/3.cpp
@@ -28,73 +28,73 @@ void build_tree(Node * & root) {
 
 
 void pre_order_traverse(Node * root) {  // 先序遍历
-	stack< Node * > s;
-	Node * p = root;
+    stack< Node * > s;
+    Node * p = root;
 
-	while (not s.empty() || p) {
-		while (p) {
-			cout << p -> data;
-			s.push(p);
-			p = p -> l;
-		}
+    while (not s.empty() || p) {
+        while (p) {
+            cout << p -> data;
+            s.push(p);
+            p = p -> l;
+        }
 
-		if (not s.empty()) {
-			p = s.top();
-			s.pop();
-			p = p -> r;
-		}
-	}
+        if (not s.empty()) {
+            p = s.top();
+            s.pop();
+            p = p -> r;
+        }
+    }
 }
 
 
 void in_order_traverse(Node * root) {   // 中序遍历
-	stack< Node * > s;
-	Node * p = root;
+    stack< Node * > s;
+    Node * p = root;
 
-	while (not s.empty() || p) {
-		while (p) {
-			s.push(p);
-			p = p -> l;
-		}
+    while (not s.empty() || p) {
+        while (p) {
+            s.push(p);
+            p = p -> l;
+        }
 
-		if (not s.empty()) {
-			p = s.top();
-			s.pop();
-			cout << p -> data;
-			p = p -> r;
-		}
-	}
+        if (not s.empty()) {
+            p = s.top();
+            s.pop();
+            cout << p -> data;
+            p = p -> r;
+        }
+    }
 }
 
 void post_order_traverse(Node * root) { // 后续遍历
-	stack< Node * > s;
-	Node * p = NULL, * pre = NULL;
+    stack< Node * > s;
+    Node * p = NULL, * pre = NULL;
 
-	s.push(root);
-	while (not s.empty()) {
-		p = s.top();
-		if ((p -> l == NULL && p -> r == NULL) || (p -> l == pre || p -> r == pre)) {
-			s.pop(), pre = p;
-			cout << p -> data;
-		}
-		else {
-			if (p -> r) s.push(p -> r);
-			if (p -> l) s.push(p -> l);
-		}
-	}
+    s.push(root);
+    while (not s.empty()) {
+        p = s.top();
+        if ((p -> l == NULL && p -> r == NULL) || (p -> l == pre || p -> r == pre)) {
+            s.pop(), pre = p;
+            cout << p -> data;
+        }
+        else {
+            if (p -> r) s.push(p -> r);
+            if (p -> l) s.push(p -> l);
+        }
+    }
 }
 
 
 int main() {
-	freopen("test_tree_3", "r", stdin);
+    freopen("test_tree_3", "r", stdin);
     Node * root = NULL;
     build_tree(root);
 
-	cout << "[pre order]  ";
-	pre_order_traverse(root);
-	cout << endl << "[in order]   ";
-	in_order_traverse(root);
-	cout << endl << "[post order] ";
-	post_order_traverse(root);
-	cout << endl;
+    cout << "[pre order]  ";
+    pre_order_traverse(root);
+    cout << endl << "[in order]   ";
+    in_order_traverse(root);
+    cout << endl << "[post order] ";
+    post_order_traverse(root);
+    cout << endl;
 }
