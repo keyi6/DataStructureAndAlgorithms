d71e9c9091aee48334cdb45333e107523a5a8b2a
cjhahaha <cj_lee_canu@hotmail.com>
2018-11-08 13:01:19 +0800

    Fix bugs

--- DynamicProgramming/matrix_chain/matrix_chain.cpp
+++ DynamicProgramming/matrix_chain/matrix_chain.cpp
@@ -12,6 +12,23 @@ const int MAXN = 100 + 10;
 
 int n, s[MAXN], dp[MAXN][MAXN], rec[MAXN][MAXN];
 
+void output(int l, int r) {
+	if (r - l <= 1) {
+		for (int i = l; i <= r; i ++)
+			cout << "A" << i << " ";
+		return;
+	}
+
+	int mid = rec[l][r];
+	cout << "(";
+	output(l, mid);
+	cout << ")";
+
+	cout << "(";
+	output(mid + 1, r);
+	cout << ")";
+}
+
 
 int main() {
 	// input
@@ -28,12 +45,13 @@ int main() {
 		for (int i = 1; i <= n - j_start + 1; i ++) {
 			int j = i + j_start - 1;
 
-			for (int k = i + 1, t; k < j; k ++) {
-				t = dp[i][k] + dp[k + 1][j] + s[i] * s[j] * s[k];
+			for (int k = i, t; k <= j; k ++) {
+				t = dp[i][k] + dp[k + 1][j] + s[i - 1] * s[j] * s[k];
 
-				if (t < dp[i][j]) dp[i][j] = t, s[i][j] = k;
+				if (t < dp[i][j]) dp[i][j] = t, rec[i][j] = k;
 			}
 		}
 
 	cout << dp[1][n] << endl;
+	output(1, n);
 }
